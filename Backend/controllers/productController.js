610246c3aeb32ab6f01f0d4ff4525bede0d06957
Tanishq Modi <tanishqmodi54@gmail.com>
2025-02-17 21:19:01 +0530

    Project backend done

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -116,8 +116,182 @@ const createProduct = asyncHandler(async (req, res) => {
         res.status(200).json({message:"product deleted successfuly"});
   });
 
+  const updateProduct = asyncHandler(async (req, res) => {
+    const { title, description, price, height, lengthpic, width, mediumused, weigth } = req.body;
+    const { id } = req.params;
+    const product = await Product.findById(id);
+  
+    if (!product) {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+    if (product.user.toString() !== req.user.id) {
+      res.status(401);
+      throw new Error("User not authorized");
+    }
+  
+    let fileData = {};
+    if (req.file) {
+      let uploadedFile;
+      try {
+        uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+          folder: "Product-Images",
+          resource_type: "image",
+        });
+      } catch (error) {
+        res.status(500);
+        throw new Error("Image colud not be uploaded");
+      }
+  
+      if (product.image && product.image.public_id) {
+        try {
+          await cloudinary.uploader.destroy(product.image.public_id);
+        } catch (error) {
+          console.error("Error deleting previous image from Cloudinary:", error);
+        }
+      }
+      //step 1 :
+      fileData = {
+        fileName: req.file.originalname,
+        filePath: uploadedFile.secure_url,
+        fileType: req.file.mimetype,
+        public_id: uploadedFile.public_id,
+      };
+    }
+  
+    const updatedProduct = await Product.findByIdAndUpdate(
+      { _id: id },
+      {
+        title,
+        description,
+        price,
+        height,
+        lengthpic,
+        width,
+        mediumused,
+        weigth,
+        image: Object.keys(fileData).length === 0 ? Product?.image : fileData,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json(updatedProduct);
+  });
+
+  const getAllProductsofUser = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+  
+    const products = await Product.find({ user: userId }).sort("-createdAt").populate("user");
+  
+    // const productsWithPrices = await Promise.all(
+    //   products.map(async (product) => {
+    //     const latestBid = await BiddingProduct.findOne({ product: product._id }).sort("-createdAt");
+    //     const biddingPrice = latestBid ? latestBid.price : product.price;
+    //     return {
+    //       ...product._doc,
+    //       biddingPrice, // Adding the price field
+    //     };
+    //   })
+    // );
+  
+    res.status(200).json(products);  //productsWithPrices
+  });
+
+  const getAllSoldProducts = asyncHandler(async (req, res) => {
+    const product = await Product.find({ isSoldout: true }).sort("-createdAt").populate("user");
+    res.status(200).json(product);
+  });
+  const getProductBySlug = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+    res.status(200).json(product);
+  });
+
+  const getWonProducts = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+  
+    const wonProducts = await Product.find({ soldTo: userId }).sort("-createdAt").populate("user");
+  
+    const productsWithPrices = await Promise.all(
+      wonProducts.map(async (product) => {
+        const latestBid = await BiddingProduct.findOne({ product: product._id }).sort("-createdAt");
+        const biddingPrice = latestBid ? latestBid.price : product.price;
+        return {
+          ...product._doc,
+          biddingPrice, // Adding the price field
+        };
+      })
+    );
+  
+    res.status(200).json(productsWithPrices);
+  });
+
+  // for admin only users
+const verifyAndAddCommissionProductByAdmin = asyncHandler(async (req, res) => {
+    const { commission } = req.body;
+    const { id } = req.params;
+  
+    const product = await Product.findById(id);
+    if (!product) {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  
+    product.isverify = true;
+    product.commission = commission;
+  
+    await product.save();
+  
+    res.status(200).json({ message: "Product verified successfully", data: product });
+  });
+
+  const getAllProductsByAmdin = asyncHandler(async (req, res) => {
+    const products = await Product.find({}).sort("-createdAt").populate("user");
+  
+    // const productsWithPrices = await Promise.all(
+    //   products.map(async (product) => {
+    //     const latestBid = await BiddingProduct.findOne({ product: product._id }).sort("-createdAt");
+    //     const biddingPrice = latestBid ? latestBid.price : product.price;
+    //     return {
+    //       ...product._doc,
+    //       biddingPrice, // Adding the price field
+    //     };
+    //   })
+    // );
+  
+    res.status(200).json(products);
+  });
+
+
+  // dot not it
+const deleteProductsByAmdin = asyncHandler(async (req, res) => {
+    try {
+      const { productIds } = req.body;
+  
+      const result = await Product.findOneAndDelete({ _id: productIds });
+  
+      res.status(200).json({ message: `${result.deletedCount} products deleted successfully` });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
 module.exports = {
   createProduct,
   getAllProducts,
   deleteProduct,
+  updateProduct,
+  getAllProductsofUser,
+  verifyAndAddCommissionProductByAdmin,
+  getAllProductsByAmdin,
+  deleteProductsByAmdin,
+  getAllSoldProducts,
+  getWonProducts,
+  getProductBySlug,
 };
--- Backend/routes/prodcutRoute.js
+++ Backend/routes/prodcutRoute.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const { createProduct,getAllProducts,deleteProduct } = require("../controllers/productController");
-const { protect } = require("../middleware/authMiddleWare");
+const { createProduct,getAllProducts,deleteProduct, updateProduct,getAllProductsofUser,verifyAndAddCommissionProductByAdmin,getAllProductsByAmdin,deleteProductsByAmdin,getAllSoldProducts,getProductBySlug,
+    getWonProducts, } = require("../controllers/productController");
+const { protect, isAdmin } = require("../middleware/authMiddleWare");
 const { isSeller } = require("../middleware/authMiddleWare");
 const { upload } = require("../utils/fileUpload");
 const router = express.Router();
 
 
 router.post("/",protect,isSeller,upload.single("image"), createProduct);
-router.get("/",getAllProducts);
 router.delete("/:id",protect,isSeller,deleteProduct);
+router.put("/:id",protect,isSeller,upload.single("image"), updateProduct);
+
+router.get("/",getAllProducts);
+router.get("/won-products", protect, getWonProducts);
+router.get("/user",protect, getAllProductsofUser );
+router.get("/sold", getAllSoldProducts);
+router.get("/:id", getProductBySlug);
+
+// Only access for admin users
+router.patch("/admin/product-verified/:id", protect, isAdmin, verifyAndAddCommissionProductByAdmin);
+router.get("/admin/products", protect, isAdmin, getAllProductsByAmdin);
+router.delete("/admin/products", protect, isAdmin, deleteProductsByAmdin);
 
 module.exports = router; 
