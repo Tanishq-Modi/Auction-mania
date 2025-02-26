14080f098d4ff5173ac5c89ef92feacf525193e6
Tanishq Modi <tanishqmodi54@gmail.com>
2025-02-26 22:10:00 +0530

    continued with frontend

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,18 +1,71 @@
-import { Container } from "./Design";
+import { menulists } from "../../assets/data";
+import { Container, CustomNavLinkList } from "./Design";
+import { IoSearchOutline } from "react-icons/io5";
+import { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+
 
 export const Header = () => {
+  const [isOpen, setisOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation();
+
+  const menuRef= useRef(null);
+
+  const toggleMenu= ()=>{
+    setisOpen(!isOpen);
+  };
+
+  const closeMenuOutSide = (event)=>{
+    if(menuRef.current && !menuRef.current.contains(event.target)){
+      setisOpen(false);
+    }
+  };
+
+  const hadleScroll = ()=>{
+    setIsScrolled(window.scrollY >0);
+  };
+
+  useEffect(()=>{
+    document.addEventListener("mousedown",closeMenuOutSide);
+    window.addEventListener("scroll",hadleScroll);
+
+    return () => {
+      document.removeEventListener("mousedown",closeMenuOutSide);
+      window.removeEventListener("scroll",hadleScroll);
+
+    };
+  }, []);
+
+  const isHomePage =location.pathname==="/";
   return (
-    <header className="header bg-primary py-4 fixed w-full top-0 left-0 z-50">
+    <header className={isHomePage ? `header py-1 bg-primary ${isScrolled ? "scrolled" : ""}` : `header bg-white shadow-s1 ${isScrolled ? "scrolled" : ""}`}>
       <Container>
-        <nav className="p-4 flex justify-between items-center">
+        <nav className="p-4 flex justify-between items-center relative">
           <div className="flex items-center gap-14">
-            <img src="../images/common/header-logo.png" alt="logo" className="h-11" />
+            <div>
+              <img src="../images/common/header-logo.png" alt="logo" className="h-11" />
+            </div>
+            <div className="hidden lg:flex items-center justify-between gap-8">
+              {menulists.map((list,index)=>(
+                <li key={index} className="capitalize list-none text-white">
+                  <CustomNavLinkList href={list.path}>{list.link}</CustomNavLinkList>
+                </li>
+              ))}
+            </div>
           </div>
           
+          <div className="flex items-center gap-8 icons">
+            <div className="hidden lg:flex lg:items-center lg:gap-8 text-white">
+            <IoSearchOutline size={23} />
+            <CustomNavLinkList href={"/seller/login"}>Become a seller</CustomNavLinkList>
+            <CustomNavLinkList href={"/login"}>Sign in</CustomNavLinkList>
+            <CustomNavLinkList href={"/register"}>Join</CustomNavLinkList>
+            </div>
+          </div>
         </nav>
       </Container>
     </header>
   );
-
-  
 };
--- client/src/components/common/Layout.jsx
+++ client/src/components/common/Layout.jsx
@@ -1,13 +1,16 @@
 import { Header, Footer } from "../../routes/index";
 import PropTypes from "prop-types";
 import { CustomNavLink } from "./Design"; // Ensure this is correctly defined
+
 // Removed incorrect import of `href` from react-router-dom
 
 export const Layout = ({ children }) => {
+
+
   return (
     <div>
       <Header />
-      <main>{children}</main>
+      <main className="h-[500vh]">{children}</main>
       <Footer />
     </div>
   );
