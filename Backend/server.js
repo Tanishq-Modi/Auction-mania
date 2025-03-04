b2194687318f3899e87231ffe1f01199c7e208d6
Tanishq Modi <tanishqmodi54@gmail.com>
2025-03-04 23:38:56 +0530

    removed env file

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 app.use(
     cors({
-        origin: ["http://localhost:3000", "Your website domain url"],
+        origin: ["http://localhost:3000"],
         credentials: true,
     })
 );
